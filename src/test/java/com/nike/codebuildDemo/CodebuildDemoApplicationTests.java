daadc4595be96bd914bd0084178cc266e2594a95
Joseph Chen <Joseph.Chen@nike.com>
2024-12-15 21:26:30 +0800

    Added some test code

--- src/test/java/com/nike/codebuildDemo/CodebuildDemoApplicationTests.java
+++ src/test/java/com/nike/codebuildDemo/CodebuildDemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.nike.codebuildDemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+//@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CodebuildDemoApplicationTests {
 
+	@Autowired
+	TestRestTemplate restTemplate;
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/b/a", String.class);
 
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
