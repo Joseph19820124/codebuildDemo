e79cfade75b8e1649e5a3b6342ed13f2874a00c0
Joseph19820124 <164839249+Joseph19820124@users.noreply.github.com>
2024-12-16 04:01:29 +0800

    Refactor to stream

--- src/main/java/com/nike/codebuildDemo/CashCardController.java
+++ src/main/java/com/nike/codebuildDemo/CashCardController.java
@@ -19,11 +19,7 @@ class CashCardController {
     public ResponseEntity<CashCard> getCashCard(@PathVariable Long requestedId) {
         System.out.println(requestedId);
         Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
-        if (cashCardOptional.isPresent()) {
-            return ResponseEntity.ok(cashCardOptional.get());
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
